383138328bdf14fe9c8fe55d5aa34b3c16902b78
Joe Minichino <joe.minichino@gmail.com>
2015-09-21 19:59:30 +0100

    fixed test

--- tests/getrange.js
+++ tests/getrange.js
@@ -4,8 +4,8 @@ let test = require('tape'),
 
 test('GETRANGE', (t) => {
   ds.set('mykey', 'foobar');
-  t.ok(ds.get('mykey'), 'foo');
+  t.equal(ds.get('mykey'), 'foobar');
 
-  t.ok(ds.getrange('mykey', 1, 4), 'ooba');
+  t.equal(ds.getrange('mykey', 1, 5), 'ooba');
   t.end();
 });
